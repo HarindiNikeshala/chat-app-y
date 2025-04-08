b06a6f978fad6aaeb9f8b15ce8082e5d90255089
harindinikeshala <harindinikeshala@gmail.com>
2025-04-08 09:37:48 +0530

    get messages commit

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -28,7 +28,7 @@ export const sendMessage = async (req, res) => {
         }
 
         //SOCKET IO
-        
+
 
         await Promise.all([conversation.save(), newMessage.save()]);
 
@@ -38,4 +38,19 @@ export const sendMessage = async (req, res) => {
         console.error("Error sending message:", error.message);
         res.status(500).json({ error: "Internal server error" });
     };
+};
+
+export const getMessages = async (req, res) => {
+    try {
+        const { id: userToChatId } = req.params;
+        const senderId = req.user._id;
+        const conversation = await Conversation.findOne({
+            participants: { $all: [senderId, userToChatId] },
+        }).populate("messages");
+
+        res.status(200).json(conversation.messages);
+    } catch (error) {
+        console.error("Error in getting messages:", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
 };
