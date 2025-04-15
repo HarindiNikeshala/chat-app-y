570d93834adc33f0f3f21704818d540beb4b486b
harindinikeshala <harindinikeshala@gmail.com>
2025-04-15 12:10:29 +0530

    Massage container commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
 import './App.css'
 import Home from './pages/home/Home'
-import SignUp from './pages/signup/SignUp'
 
 function App() {
-
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
       <Home />
