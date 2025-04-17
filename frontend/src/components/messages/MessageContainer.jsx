c0bec128cead4a91c26f7406c617726851243fd7
harindinikeshala <harindinikeshala@gmail.com>
2025-04-17 19:58:37 +0530

    Get conversation commit

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Messages from './Messages'
 import MessageInput from './MessageInput'
 import { TiMessages } from 'react-icons/ti'
+import useConversation from '../../zustand/useConversation'
 
 const MessageContainer = () => {
-    const noChatSelected = true;
+    const { selectedConversation, setSelectedConversation } = useConversation();
+
+    useEffect(() => {
+        return () => setSelectedConversation(null);
+    }, [setSelectedConversation])
+
     return (
         <div className='md:min-w-[450px] flex flex-col'>
-            {noChatSelected ? (
+            {!selectedConversation ? (
                 <NoChatSelected />
             ) : (
                 <>
-                    <div className='bg-green-900 px-4 py-2 mb-2'>
-                        <span className='label-text'>To:</span>{" "}
-                        <span className='text-white font-bold'>Tharaka Prabhath</span>
+                    <div className='bg-green-700 px-4 py-2 mb-2'>
+                        <div className='flex items-center gap-4'>
+                            <div className='w-12 rounded-full'>
+                                <img src={selectedConversation.profilePic} alt="user avatar" />
+                            </div>
+                            <span className='text-white font-bold'>{selectedConversation.fullName}</span>
+                        </div>
                     </div>
                     <Messages />
                     <MessageInput />
