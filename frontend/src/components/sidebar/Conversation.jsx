e2ce4b79fab6f74e63cce50db5283688676f6216
harindinikeshala <harindinikeshala@gmail.com>
2025-04-23 00:57:30 +0530

    Send message functionality commit

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import useConversation from '../../zustand/useConversation';
 
-const Conversation = ({ conversation, lastIdx, emoji }) => {
+const Conversation = ({ conversation, lastIdx }) => {
     const { selectedConversation, setSelectedConversation } = useConversation();
     const isSelected = selectedConversation?._id === conversation._id;
     return (
